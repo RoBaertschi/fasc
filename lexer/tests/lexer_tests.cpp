fa096c22fea4ef5c4a2c6d35a43d98c3267699b3
Robin <rtmbaertschi007@gmail.com>
2024-08-03 21:52:20 +0200

    more lexer tests

--- lexer/tests/lexer_tests.cpp
+++ lexer/tests/lexer_tests.cpp
@@ -9,3 +9,27 @@ TEST_CASE("Test Comments", "[lexer]") {
     auto lexer = Lexer("// Test");
     REQUIRE(lexer.nextToken().type == TokenType::Eof);
 }
+
+struct TestCase {
+    std::string input;
+    std::vector<Token> expectedTokens;
+};
+
+TEST_CASE("Test valid tokens", "[lexer]") {
+    for (std::vector<TestCase> testCases{
+             {
+                 "",
+                 {{"",TokenType::Eof}}
+             }
+         }; auto [input, expectedTokens]: testCases) {
+        auto lexer = Lexer(input);
+
+        int i = 0;
+        for (const auto token = lexer.nextToken(); token.type != TokenType::Eof; i++) {
+           REQUIRE(token.type == expectedTokens[i].type);
+           REQUIRE(token.literal == expectedTokens[i].literal);
+        }
+
+        REQUIRE(i + 1 == expectedTokens.size());
+   }
+}
